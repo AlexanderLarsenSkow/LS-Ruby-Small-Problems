04c7bd473ec3d2c1e2c058113d591aa82fdcc4a2
Alex Skow <askow2015@gmail.com>
2024-06-12 05:53:54 +0000

    Finished Even Numbers Exercise

---
+++ easy2/even_numbers.rb
@@ -0,0 +1,23 @@
+=begin 
+
+Print all even numbers on separate lines.
+
+Rules: must all be even.
+
+Expected Input: range of numbers
+
+Expected output: printed numbers
+
+Types of data: range
+
+=end 
+
+numbers = 1..99
+
+#numbers.each{|n| puts n if n.even?}
+
+1.upto(99) {|n| puts n if n % 2 == 0}
+
+#array = numbers.select{|n| n.even?}
+
+#puts array
