50d3641a260bfdaa0c67aad50bf5a5536b17a760
Alex Skow <askow2015@gmail.com>
2024-06-18 20:19:13 +0000

    Finished Exercise 9

---
+++ documentation/9.rb
@@ -0,0 +1,7 @@
+# Find the documentation for YAML::load_file
+
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+# Can find lots of information about YAML in the Psych Module on the docs. Search psych load_file
+
